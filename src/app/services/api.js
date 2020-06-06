971c63320c88309e9691c2baf7e9c4cf0bf21725
Henrique Augusto <hick_97@hotmail.com>
2020-06-06 18:33:40 -0300

    feat: add alpha vantage url

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,5 +1,7 @@
 const TELEGRAM_KEY = '1099734412:AAGpSWaNzMgLWMOpZuHjwbU0Ja2ky_FqsOo'
+const ALPHA_VANTAGE_KEY = 'SLI295BQJVUNQHVL'
 
 module.exports = {
-  baseURL: `https://api.telegram.org/bot${TELEGRAM_KEY}`
+  telegramURL: `https://api.telegram.org/bot${TELEGRAM_KEY}`,
+  alphaVantageURL: `https://www.alphavantage.co/query?apikey=${ALPHA_VANTAGE_KEY}`
 }
