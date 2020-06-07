2dc6c5ed948029490bd0e6a65093577c2f3a6214
Henrique Augusto <hick_97@hotmail.com>
2020-06-07 12:19:40 -0300

    fix: message response

--- src/app/enum/messages.js
+++ src/app/enum/messages.js
@@ -1,7 +1,5 @@
-const startMessage = require('../markdowns/startMessage.md')
-
 const messages = {
-  START_MESSAGE: startMessage,
+  START_MESSAGE: '# \U0001F4C8 @BRStocksBot\n\n' + 'Bem-vindo ao BR Stocks Bot!\n' + 'Monte sua carteira para monitorar ativos que mais lhe interessam.\n' + 'Para adicionar um novo ativo na sua carteira, siga a descrição abaixo:\n' + '<i>/stock Ativo Quantidade PM(Preço médio)</i>\n' + 'Para mais informações, insira nosso comando <i>/help</i>',
   helpMessage: '',
   removeStock: '',
   errorMessage: '',
