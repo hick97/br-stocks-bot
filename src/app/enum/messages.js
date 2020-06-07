e9fea704e0a319a77ac09a080643194b233b4e47
Henrique Augusto <hick_97@hotmail.com>
2020-06-07 14:32:59 -0300

    feat: updating messages

--- src/app/enum/messages.js
+++ src/app/enum/messages.js
@@ -1,6 +1,6 @@
 const messages = {
   START_MESSAGE: '<b>&#x1F4C8 @BRStocksBot</b>\n\n' + 'Bem-vindo ao <b>BR Stocks Bot</b>!\n\n' + 'Monte sua carteira para monitorar seus ativos favoritos.\n\n' + 'É simples de usar:\n\n' + '&#x1F449 Adicione um novo ativo:\n' + '<i>/stock Ativo Quantidade PM</i>\n\n' + '&#x1F449 Obtenha detalhes da carteira:\n' + '<i>/details</i>\n\n' + 'Dúvidas? Mais informações em <i>/help</i>.\n\n',
-  HELP_MESSAGE: '<b>&#x2753 Instruções</b>\n\n' + '<b>&#x1F449 Ativos</b>\n\n' + 'Cadastrar ativo ou editar ativo: \n' + '<i>/stock Ativo Quantidade PM</i>\n\n' + 'Deletar ativo:\n' + '<i>/del Ativo</i>\n\n' + '<b>&#x1F449 Carteira</b>\n\n' + 'Visualizar ativos:\n' + '<i>/wallet</i>\n\n' + 'Detalhes da carteira:\n' + '<i>/details</i>\n\n' + 'By: <a href="https://github.com/hick97">@hick97</a>',
+  HELP_MESSAGE: '<b>&#x1F4C8 @BRStocksBot</b>\n\n' + '<b>&#x2753 Instruções</b>\n\n' + '<b>&#x1F449 Cadastrar ativo ou editar ativo: </b>\n' + '/stock <i>Ativo Quantidade PM</i>\n\n' + '<b>&#x1F449 Deletar ativo:</b>\n' + '/del <i>Ativo</i>\n\n' + '<b>&#x1F449 Visualizar ativos da carteira:</b>\n' + '/wallet\n\n' + '<b>&#x1F449 Detalhes da carteira:</b>\n' + '/details\n\n' + 'By: <a href="https://github.com/hick97">@hick97</a>',
   removeStock: '',
   errorMessage: '',
   notFound: ''
