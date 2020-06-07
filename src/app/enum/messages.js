2bc8f86e29755ab6e38e7c46e609e82bf50da842
Henrique Augusto <hick_97@hotmail.com>
2020-06-07 12:58:32 -0300

    fix: tags on start message

--- src/app/enum/messages.js
+++ src/app/enum/messages.js
@@ -1,5 +1,5 @@
 const messages = {
-  START_MESSAGE: '<b>&#x1F4C8 @BRStocksBot</b>\n\n' + 'Bem-vindo ao BR Stocks Bot!\n' + 'Monte sua carteira para monitorar ativos que mais lhe interessam.\n' + 'Para adicionar um novo ativo na sua carteira, siga a descrição abaixo:\n' + '<i>/stock Ativo Quantidade PM(Preço médio)</i>\n' + 'Para mais informações, insira nosso comando <i>/help</i>',
+  START_MESSAGE: '<b>@BRStocksBot</b>\n\n' + 'Bem-vindo ao BR Stocks Bot!\n' + 'Monte sua carteira para monitorar ativos que mais lhe interessam.\n' + 'Para adicionar um novo ativo na sua carteira, siga a descrição abaixo:\n' + '<i>stock Ativo Quantidade PM(Preço médio)</i>\n' + 'Para mais informações, insira nosso comando <i>help</i>',
   helpMessage: '',
   removeStock: '',
   errorMessage: '',
