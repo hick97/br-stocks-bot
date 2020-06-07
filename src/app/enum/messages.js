180678e7881b9cceaff63242dfdeb6795ad01a3a
Henrique Augusto <hick_97@hotmail.com>
2020-06-07 13:36:14 -0300

    chore: updating start message

--- src/app/enum/messages.js
+++ src/app/enum/messages.js
@@ -1,5 +1,5 @@
 const messages = {
-  START_MESSAGE: '<b>&#x1F4C8 @BRStocksBot</b>\n\n' + 'Bem-vindo ao <b>BR Stocks Bot</b>!\n\n' + 'Monte sua carteira para monitorar ativos que mais lhe interessam.\n\n' + 'É simples de usar:\n\n' + '&#x0031 &#x20E3 Adicione um novo ativo:\n' + '<i>/stock Ativo Quantidade PM</i>\n\n' + '&#x0032 &#x20E3 Obtenha informações sobre sua carteira:\n' + '<i>/details</i>\n\n' + 'Para mais informações, insira nosso comando <i>/help</i>',
+  START_MESSAGE: '<b>&#x1F4C8 @BRStocksBot</b>\n\n' + 'Bem-vindo ao <b>BR Stocks Bot</b>!\n\n' + 'Monte sua carteira para monitorar seus ativos favoritos.\n\n' + 'É simples de usar:\n\n' + '&#x1F449 Adicione um novo ativo:\n' + '<i>/stock Ativo Quantidade PM</i>\n\n' + '&#x1F449 Obtenha informações sobre sua carteira:\n' + '<i>/details</i>\n\n' + 'Dúvidas? Mais informações em <i>/help</i>.\n\n' + 'By: Henrique Augusto',
 
   helpMessage: '',
   removeStock: '',
