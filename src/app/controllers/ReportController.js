5fee917ee2c00ce7d6df09b1e03c3697524852f8
Henrique Augusto <hick_97@hotmail.com>
2020-07-22 22:53:36 -0300

    fix: updato to prd

--- src/app/controllers/ReportController.js
+++ src/app/controllers/ReportController.js
@@ -18,23 +18,23 @@ class ReportController {
       for (let index = 0; index < subscriptions.length; index++) {
         let stocksReport = null
         let walletReport = null
-        if (subscriptions[index].chat_id === 680912149) {
-          stocksReport = await buildReport(subscriptions[index].chat_id, subscriptions[index].stocks)
-          walletReport = await buildWalletReport(subscriptions[index].stocks, stocksReport)
-        }
+        // if (subscriptions[index].chat_id === 680912149) {
+        stocksReport = await buildReport(subscriptions[index].chat_id, subscriptions[index].stocks)
+        walletReport = await buildWalletReport(subscriptions[index].stocks, stocksReport)
+        // }
 
         // console.log(stocksReport)
-        if (subscriptions[index].chat_id === 680912149) {
-          // await sendMessage(680912149, walletReport)
-          const { stocks, fiis, others } = stocksReport.message
+        // if (subscriptions[index].chat_id === 680912149) {
+        // await sendMessage(680912149, walletReport)
+        const { stocks, fiis, others } = stocksReport.message
 
-          await sendMessage(subscriptions[index].chat_id, walletReport)
-          stocks.length > 0 && await sendMessage(stocksReport.chat_id, stocks)
-          fiis.length > 0 && await sendMessage(stocksReport.chat_id, fiis)
-          others.length > 0 && await sendMessage(stocksReport.chat_id, '<b>OUTROS</b>\n\n' + others + '<code>Atenção: Até o momento, o @brstocksbot suporta apenas as seguintes classes de ativos: AÇÕES ou Fundo de Investimento Imobiliário. Em breve daremos suporte a ETFs também &#x1F916</code>')
+        await sendMessage(subscriptions[index].chat_id, walletReport)
+        stocks.length > 0 && await sendMessage(stocksReport.chat_id, stocks)
+        fiis.length > 0 && await sendMessage(stocksReport.chat_id, fiis)
+        others.length > 0 && await sendMessage(stocksReport.chat_id, '<b>OUTROS</b>\n\n' + others + '<code>Atenção: Até o momento, o @brstocksbot suporta apenas as seguintes classes de ativos: AÇÕES ou Fundo de Investimento Imobiliário. Em breve daremos suporte a ETFs também &#x1F916</code>')
 
-          // await sendMessage(680912149, stocksReport.message)
-        }
+        // await sendMessage(680912149, stocksReport.message)
+        // }
       }
     } catch (err) {
       useSentryLogger(err)
