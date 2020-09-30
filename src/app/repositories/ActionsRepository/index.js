9e4b2bee6f2dddca1192742a43c95d8b58220ffc
Henrique Augusto <hick_97@hotmail.com>
2020-09-30 20:54:01 -0300

    improvement: improve Actions repository with new methods

--- src/app/repositories/ActionsRepository/index.js
+++ src/app/repositories/ActionsRepository/index.js
@@ -1,13 +1,32 @@
+const { stockIsValid } = require('../StockRepository')
+const { updateWallet, listWalletById } = require('../WalletRepository')
 const { isSingleCommand } = require('../MessageRepository')
+const { isFundamentalsRequest } = require('../FundamentalsRepository')
+
+const FundamentalsController = require('../../controllers/FundamentalsController')
 
 const singleCommands = require('../../helpers/singleCommandsFunc')
 
 class ActionsRepository {
+  getAction(message) {
+    const { text } = message
+
+    return text.split(' ')[0].trim()
+  }
+
   async staticMessage(message) {
     return isSingleCommand(message) && singleCommands[message.text]
   }
 
-  async handleWallet(message) { }
+  async handleWallet(message) {
+    return message.text === '/wallet'
+      ? await listWalletById(message.text)
+      : stockIsValid(message) && await updateWallet(message)
+  }
+
+  async handleFundamentals(message) {
+    return await isFundamentalsRequest(message) && await FundamentalsController.execute(message)
+  }
 }
 
 module.exports = new ActionsRepository()
