3ed08bb3be7cabf8e9140b101c477a16a2cac216
Henrique Augusto <hick_97@hotmail.com>
2020-09-30 20:55:30 -0300

    refactor: using Actions approach in MessageController

--- src/app/controllers/MessageController.js
+++ src/app/controllers/MessageController.js
@@ -1,67 +1,39 @@
 const { sendMessage, sendGifAnimation } = require('../repositories/MessageRepository')
-const { stockIsValid } = require('../repositories/StockRepository')
-const { isFundamentalsRequest } = require('../repositories/FundamentalsRepository')
-const { updateWallet, listWalletById } = require('../repositories/WalletRepository')
 
-const FundamentalsController = require('./FundamentalsController')
+const Actions = require('../repositories/ActionsRepository')
 
 const { useSentryLogger } = require('../helpers/exceptionHelper')
 const { needAnimation } = require('../helpers/animationHelper')
 
 const staticMessages = require('../enum/messages')
 
-const Actions = require('../repositories/ActionsRepository')
-
 class MessageController {
   async execute(req, res) {
-    let { message, edited_message } = req.body
-    let text = null
+    const { message, edited_message } = req.body
+    const command = message || edited_message || null
+
+    const ActionsHandler = {
+      '/start': Actions.staticMessage,
+      '/help': Actions.staticMessage,
+      '/stock': Actions.handleWallet,
+      '/del': Actions.handleWallet,
+      '/fundamentals': Actions.handleFundamentals,
+      default: staticMessages.INVALID_COMMAND
+    }
 
     try {
-      if (!message) message = edited_message
-      if (!message) throw Error('Ocorreceu um erro com a mensagem recebida, tente novamente em alguns instantes.')
+      const action = Actions.getAction(command)
+      const response = await ActionsHandler[action](command) || ActionsHandler.default
 
-      // Done
-      text = await Actions.staticMessage(message)
+      await sendMessage(message.chat.id, response, message.message_id)
+      needAnimation(message.text) && await sendGifAnimation({
+        chat_id: message.chat.id,
+        fileName: 'gif-start'
+      })
 
-      if (text) {
-        await sendMessage(message.chat.id, text, message.message_id)
-        needAnimation(message.text) && await sendGifAnimation({
-          chat_id: message.chat.id,
-          fileName: 'gif-start'
-        })
-        return res.json({ text })
-      }
-
-      // TODO
-      text = stockIsValid(message) && await updateWallet(message)
-      if (text) {
-        await sendMessage(message.chat.id, text, message.message_id)
-        return res.json({ text })
-      }
-
-      text = await isFundamentalsRequest(message) && await FundamentalsController.execute(message)
-
-      if (text) {
-        await sendMessage(message.chat.id, text, message.message_id)
-        return res.json({ text })
-      }
-
-      switch (message.text) {
-        case '/wallet':
-          text = await listWalletById(message.chat.id)
-          break
-
-        default:
-          text = staticMessages.INVALID_COMMAND
-          break
-      }
-
-      await sendMessage(message.chat.id, text, message.message_id)
-      return res.json({ text })
+      return res.json({ result: response })
     } catch (err) {
       useSentryLogger(err)
-      // message && await sendMessage(message.chat.id, staticMessages.ERROR_MESSAGE)
       return res.json({ error: err.message })
     }
   }
