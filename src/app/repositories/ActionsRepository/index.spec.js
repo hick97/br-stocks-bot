2481d4d644e29ab31456d3130fcf9cfd28ae8827
Henrique Augusto <hick_97@hotmail.com>
2020-09-30 20:54:26 -0300

    test: add new tests to Actions repository

--- src/app/repositories/ActionsRepository/index.spec.js
+++ src/app/repositories/ActionsRepository/index.spec.js
@@ -20,11 +20,24 @@ describe('Actions', () => {
     expect(result).toMatch(singleCommands['/start'])
   })
 
-  it('staticMessage -> should return a falsy value to invalid commands', async () => {
+  it('getAction -> should return the correct action value given a command', () => {
+    const message = { text: '/stock ITSA3 10 5.50' }
+
+    const result = Actions.getAction(message)
+
+    expect(result).toBeTruthy()
+    expect(result).toMatch('/stock')
+  })
+
+  it('should return a falsy value to invalid commands', async () => {
     const message = { text: '/invalid' }
 
-    const result = await Actions.staticMessage(message)
+    const handleWalletResult = await Actions.handleWallet(message)
+    const staticMessageResult = await Actions.staticMessage(message)
+    const handleFundamentalsResult = await Actions.handleFundamentals(message)
 
-    expect(result).toBeFalsy()
+    expect(handleWalletResult).toBeFalsy()
+    expect(staticMessageResult).toBeFalsy()
+    expect(handleFundamentalsResult).toBeFalsy()
   })
 })
