07addf471990749b02c2e30b13dc11a67880923d
Henrique Augusto <hick_97@hotmail.com>
2020-11-03 20:50:04 -0300

    improvement: improve FundamentalsController

--- src/app/controllers/FundamentalsController.js
+++ src/app/controllers/FundamentalsController.js
@@ -1,39 +1,38 @@
 const FundamentalsRepository = require('../repositories/FundamentalsRepository')
 const staticMessages = require('../enum/messages')
+
 const { useSentryLogger } = require('../helpers/exceptionHelper')
+const { splitCommand } = require('../helpers/CommandHelper')
 
 class FundamentalsController {
   async execute(message) {
     const { text, chat } = message
 
-    const keys = text.split(' ').map(function (item) {
-      return item.trim()
-    })
-    const values = keys.filter(element => element !== '')
+    const values = splitCommand(text)
 
-    if (values.length === 1 || values.length > 2) return staticMessages.INVALID_COMMAND
+    const numberOfParams = 2
+    const isValid = values.length === numberOfParams
+    if (!isValid) return staticMessages.INVALID_COMMAND
 
     const symbol = values[1]
-
     const fundamentals = await FundamentalsRepository.getFundamentalsByStock(symbol)
-
     const isInvalidStock = !fundamentals || fundamentals.length === 0
 
     if (isInvalidStock) {
-      useSentryLogger(null, `Error(chat_id=${chat.id}) - Não foi possível achar fundamentos para o ativo=${symbol}.`)
+      const errorMessage = `Error(chat_id=${chat.id}) - Não foi possível achar fundamentos para o ativo=${symbol}).`
+      useSentryLogger(null, errorMessage)
       return staticMessages.NOT_FOUND
     }
 
-    const fundamentalsText = await FundamentalsRepository.getFundamentalsText(fundamentals, symbol)
-
-    return fundamentalsText
+    return await FundamentalsRepository.getFundamentalsText(fundamentals, symbol)
   }
 
   async updateFundamentals() {
     try {
       await FundamentalsRepository.updateAllFundamentals()
     } catch (err) {
-      useSentryLogger(err)
+      const errorMessage = 'Error (Houve uma falha ao atualizar todos os fundamentos)'
+      useSentryLogger(err, errorMessage)
     }
   }
 }
