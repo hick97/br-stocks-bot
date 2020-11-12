10490f5282194ac56c4f08d873ef5f8558dd3ebf
Henrique Augusto <hick_97@hotmail.com>
2020-11-11 21:00:29 -0300

    improvement: remove unecessary methods on report helper

--- src/app/helpers/ReportHelper/index.js
+++ src/app/helpers/ReportHelper/index.js
@@ -1,9 +1,7 @@
 const { isNegativeCheck, parseToFixedFloat } = require('../CurrencyHelper')
 const { emojis } = require('../../enum/EmojiEnum')
 
-const getPartialRentability = (initialAmount = 0, currentAmount = 0) => parseToFixedFloat((currentAmount - initialAmount) / initialAmount * 100)
-
-const getStockReportText = (symbol, stockData, difference = 0, partial = 0, partialRentability = 0) => {
+const getStockReportText = (symbol, stockData, dailyCurrencyVariation = 0, partialResult = 0, partialPercentualVariation = 0) => {
   const isNegative = isNegativeCheck(stockData.change)
   const defaultEmojis = isNegative ? emojis.brokenHeart : emojis.greenHeart
 
@@ -12,8 +10,8 @@ const getStockReportText = (symbol, stockData, difference = 0, partial = 0, part
 
   const withoutError =
     '<code>FECHAM.</code>\t\t\t<code>R$ ' + stockData.price + '</code>\n' +
-    '<code>RENTAB.</code>\t\t\t<code>R$ ' + parseToFixedFloat(difference) + ' (' + stockData.change + ')' + '</code>\n' +
-    '<code>PARCIAL</code>\t\t\t<code>R$ ' + parseToFixedFloat(partial) + ' (' + partialRentability + '%)' + '</code>\n\n'
+    '<code>RENTAB.</code>\t\t\t<code>R$ ' + parseToFixedFloat(dailyCurrencyVariation) + ' (' + stockData.change + ')' + '</code>\n' +
+    '<code>PARCIAL</code>\t\t\t<code>R$ ' + parseToFixedFloat(partialResult) + ' (' + partialPercentualVariation + '%)' + '</code>\n\n'
 
   const report = stockData.failed ? withError : withoutError
   const customIcon = stockData.failed ? emojis.prohibited : defaultEmojis
@@ -21,24 +19,4 @@ const getStockReportText = (symbol, stockData, difference = 0, partial = 0, part
   return '<b>' + symbol.toUpperCase() + '</b> ' + customIcon + '\n' + report
 }
 
-const getCurrentDate = (withHTML = true) => {
-  var today = new Date()
-  var dd = String(today.getDate()).padStart(2, '0')
-  var mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
-  var yyyy = today.getFullYear()
-
-  const currentDate = dd + '/' + mm + '/' + yyyy
-
-  return withHTML ? ('<b>' + emojis.calendar + ' ' + currentDate + '</b>') : currentDate
-}
-
-const shareByWhatsapp = (text) => ({
-  inline_keyboard: [
-    [{
-      text: 'Compartilhar - Whatsapp',
-      url: `https://api.whatsapp.com/send?text=${text}`
-    }]
-  ]
-})
-
-module.exports = { getStockReportText, getPartialRentability, getCurrentDate, shareByWhatsapp }
+module.exports = { getStockReportText }
