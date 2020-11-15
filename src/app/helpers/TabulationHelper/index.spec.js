97cb7ebaea626a381f22e980e0352debef293e25
Henrique Augusto <hick_97@hotmail.com>
2020-11-15 16:51:29 -0300

    test: add test to walletTabulation in tabulation helper

--- src/app/helpers/TabulationHelper/index.spec.js
+++ src/app/helpers/TabulationHelper/index.spec.js
@@ -1,5 +1,5 @@
-const { fundamentalsTabulation } = require('./index')
-const { fundamentalsTabulationMock } = require('./mocks/index')
+const { fundamentalsTabulation, walletTabulation } = require('./index')
+const { fundamentalsTabulationMock, walletTabulationMock } = require('./mocks/index')
 
 describe('TabulationHelper', () => {
   it('fundamentalsTabulation -> should return the correct tabulation by string length', async () => {
@@ -20,5 +20,25 @@ describe('TabulationHelper', () => {
     expect(resultWithLengthThree).toBe(fundamentalsTabulationMock[3])
     expect(resultWithLengthTwo).toBe(fundamentalsTabulationMock[2])
     expect(resultWithLengthOne).toBe(fundamentalsTabulationMock[1])
+  })
+
+  it('walletTabulation -> should return the correct tabulation by string length', async () => {
+    const empty = ''
+    const resultWithLengthSeven = walletTabulation(7)
+    const resultWithLengthSix = walletTabulation(6)
+    const resultWithLengthFive = walletTabulation(5)
+    const resultWithLengthFour = walletTabulation(4)
+    const resultWithLengthThree = walletTabulation(3)
+    const resultWithLengthTwo = walletTabulation(2)
+    const resultWithLengthOne = walletTabulation(1)
+
+    expect(resultWithLengthSeven).toBe(empty)
+
+    expect(resultWithLengthSix).toBe(walletTabulationMock[6])
+    expect(resultWithLengthFive).toBe(walletTabulationMock[5])
+    expect(resultWithLengthFour).toBe(walletTabulationMock[4])
+    expect(resultWithLengthThree).toBe(walletTabulationMock[3])
+    expect(resultWithLengthTwo).toBe(walletTabulationMock[2])
+    expect(resultWithLengthOne).toBe(walletTabulationMock[1])
   })
 })
