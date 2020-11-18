13ead64173ecff58f54adfa52f7c08ea451467de
Henrique Augusto <hick_97@hotmail.com>
2020-11-18 16:10:26 -0300

    feat: add text helpers on ReportHelper

--- src/app/helpers/ReportHelper/index.js
+++ src/app/helpers/ReportHelper/index.js
@@ -1,22 +1,48 @@
-const { isNegativeCheck, parseToFixedFloat } = require('../CurrencyHelper')
+const { isNegativeCheck, parseToFixedFloat, getPartialRentability } = require('../CurrencyHelper')
+const { getCurrentDate } = require('../DateHelper')
+
 const { emojis } = require('../../enum/EmojiEnum')
 
-const getStockReportText = (symbol, stockData, dailyCurrencyVariation = 0, partialResult = 0, partialPercentualVariation = 0) => {
-  const isNegative = isNegativeCheck(stockData.change)
+const getStockReportText = ({ symbol, dailyResult, dailyCurrencyVariation, partialResult, partialPercentualVariation }) => {
+  const isNegative = isNegativeCheck(dailyResult.change)
   const defaultEmojis = isNegative ? emojis.brokenHeart : emojis.greenHeart
 
-  const withError =
-    '<code>ATIVO INVÁLIDO</code>\n\n'
-
   const withoutError =
-    '<code>FECHAM.</code>\t\t\t<code>R$ ' + stockData.price + '</code>\n' +
-    '<code>RENTAB.</code>\t\t\t<code>R$ ' + parseToFixedFloat(dailyCurrencyVariation) + ' (' + stockData.change + ')' + '</code>\n' +
+    '<code>FECHAM.</code>\t\t\t<code>R$ ' + dailyResult.price + '</code>\n' +
+    '<code>RENTAB.</code>\t\t\t<code>R$ ' + parseToFixedFloat(dailyCurrencyVariation) + ' (' + dailyResult.change + ')' + '</code>\n' +
     '<code>PARCIAL</code>\t\t\t<code>R$ ' + parseToFixedFloat(partialResult) + ' (' + partialPercentualVariation + '%)' + '</code>\n\n'
 
-  const report = stockData.failed ? withError : withoutError
-  const customIcon = stockData.failed ? emojis.prohibited : defaultEmojis
-
-  return '<b>' + symbol.toUpperCase() + '</b> ' + customIcon + '\n' + report
+  return '<b>' + symbol.toUpperCase() + '</b> ' + defaultEmojis + '\n' + withoutError
 }
 
-module.exports = { getStockReportText }
+const getStockReportTextWhenFailed = ({ symbol }) => {
+  const withError = '<code>ATIVO INVÁLIDO</code>\n\n'
+  const formattedSymbol = symbol.toUpperCase()
+
+  const text = '<b>' + formattedSymbol + '</b> ' + emojis.prohibited + '\n' + withError
+
+  return text
+}
+
+const getCompleteReportByClass = ({ shares, type, emoji }) => {
+  const reportHour = getCurrentDate() + '<code> ( 17h50 )</code>\n\n'
+  const textHeader = `<b>${emojis[emoji]} ${type}</b>\n\n`
+
+  const partialText = shares.map(item => {
+    const partialRentability = getPartialRentability(item.initialAmount, item.partialResult)
+    const currentTex =
+      getStockReportText({
+        symbol: item.stock,
+        dailyResult: item.dailyResult,
+        dailyCurrencyVariation: item.dailyCurrencyVariation,
+        partialResult: item.partialResult,
+        partialPercentualVariation: partialRentability
+      })
+
+    return currentTex
+  })
+
+  return reportHour + textHeader + partialText.join('')
+}
+
+module.exports = { getStockReportText, getStockReportTextWhenFailed, getCompleteReportByClass }
