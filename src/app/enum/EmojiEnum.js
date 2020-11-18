d3e14165306f47e34bcf7fcca98e1aeb0cca394c
Henrique Augusto <hick_97@hotmail.com>
2020-11-18 15:48:17 -0300

    feat: add graphic and building in emoji enum

--- src/app/enum/EmojiEnum.js
+++ src/app/enum/EmojiEnum.js
@@ -2,7 +2,9 @@ const emojis = {
   greenHeart: '&#x1F49A',
   brokenHeart: '&#x1F494',
   prohibited: '&#x1F6AB',
-  calendar: '&#x1F4C5'
+  calendar: '&#x1F4C5',
+  graphic: '&#x1F4CA',
+  building: '&#x1F3E2'
 }
 
 module.exports = { emojis }
