4dba4102df9753df64fb401e4966eff6173b7f59
Henrique Augusto <hick_97@hotmail.com>
2020-11-29 16:29:49 -0300

    feat: create an enum to Stock attributes

---
+++ src/app/repositories/StockRepository/utils/stockAttributes.js
@@ -0,0 +1,13 @@
+const stockAttributes = {
+  SYMBOL: '1. symbol',
+  NAME: '2. name',
+  TYPE: '3. type',
+  REGION: '4. region',
+  MARKET_OPEN: '5. marketOpen',
+  MARKET_CLOSE: '6. marketClose',
+  TIMEZONE: '7. timezone',
+  CURRENCY: '8. currency',
+  MATCH_SCORE: '9. matchScore'
+}
+
+module.exports = { stockAttributes }
