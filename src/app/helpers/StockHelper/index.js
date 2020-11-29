80e973d8cc78a05b47377c8ca35c985ba25b9ba7
Henrique Augusto <hick_97@hotmail.com>
2020-11-29 16:18:16 -0300

    feat: move getStockValues to StockHelper

--- src/app/helpers/StockHelper/index.js
+++ src/app/helpers/StockHelper/index.js
@@ -1,3 +1,5 @@
+const { splitCommand } = require('../CommandHelper')
+
 const validStockCommand = (term) => {
   const regex = /\/stock|\/del/i
   const match = term.search(regex)
@@ -7,4 +9,22 @@ const validStockCommand = (term) => {
   return matched
 }
 
-module.exports = { validStockCommand }
+const getStockValues = (text) => {
+  const allowedPropsLength = [2, 4]
+  const values = splitCommand(text)
+  const hasCorrectLength = allowedPropsLength.includes(values.length)
+
+  const empty = {}
+  if (!hasCorrectLength) return empty
+
+  const [actions, stock, quantity, price] = values
+
+  return {
+    actions,
+    stock,
+    quantity,
+    price
+  }
+}
+
+module.exports = { validStockCommand, getStockValues }
