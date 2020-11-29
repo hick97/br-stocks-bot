4f3e251caf6c1285e6356e201cb73d55843ab205
Henrique Augusto <hick_97@hotmail.com>
2020-11-29 16:18:47 -0300

    test: add tests to getStockValues helper

--- src/app/helpers/StockHelper/index.spec.js
+++ src/app/helpers/StockHelper/index.spec.js
@@ -1,4 +1,4 @@
-const { validStockCommand } = require('./index')
+const { validStockCommand, getStockValues } = require('./index')
 
 describe('FundamentalsHelper', () => {
   it('validStockCommand -> should return true when term contains /del', async () => {
@@ -27,5 +27,31 @@ describe('FundamentalsHelper', () => {
     expect(lowerCaseMatch).toBeTruthy()
     expect(upperCaseMatch).toBeTruthy()
     expect(similarMatch).toBeTruthy()
+  })
+
+  it('getStockValues -> should return the correct values given a /stock or /del command', async () => {
+    const commandNewStock = '/stock ITSA4 15 10.48'
+    const commandDeleteStock = '/del ITSA4'
+    const invalidCommand = '/stock ITSA4 10.48'
+
+    const newStockResult = getStockValues(commandNewStock)
+    const deleteStockResult = getStockValues(commandDeleteStock)
+    const invalidCommandResult = getStockValues(invalidCommand)
+
+    expect(newStockResult).toStrictEqual(
+      {
+        actions: '/stock',
+        stock: 'ITSA4',
+        quantity: '15',
+        price: '10.48'
+      }
+    )
+    expect(deleteStockResult).toStrictEqual({
+      actions: '/del',
+      stock: 'ITSA4',
+      quantity: undefined,
+      price: undefined
+    })
+    expect(invalidCommandResult).toEqual({})
   })
 })
