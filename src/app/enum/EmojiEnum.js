03a60b403c0b6995896b6d13e410f55ae3ba23e0
Henrique Augusto <hick_97@hotmail.com>
2020-12-09 20:27:48 -0300

    feat: add new emojis on emojis enum

--- src/app/enum/EmojiEnum.js
+++ src/app/enum/EmojiEnum.js
@@ -5,7 +5,13 @@ const emojis = {
   calendar: '&#x1F4C5',
   graphic: '&#x1F4CA',
   building: '&#x1F3E2',
-  moneyBag: '&#x1F4B0'
+  moneyBag: '&#x1F4B0',
+  warning: '&#x26A0',
+  chartUpwards: '&#x1F4C8',
+  rightPointing: '&#x1F449',
+  mobilePhone: '&#x1F4F1',
+  openBook: '&#x1F4D6',
+  bankNote: '&#x1F4B5'
 }
 
 module.exports = { emojis }
