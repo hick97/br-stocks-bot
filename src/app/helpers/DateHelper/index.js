f4a40675c5dd097e5a86ae2d1aba78a075c8f393
Henrique Augusto <hick_97@hotmail.com>
2021-01-07 21:06:10 -0300

    feat: add helper to check if is weekend

--- src/app/helpers/DateHelper/index.js
+++ src/app/helpers/DateHelper/index.js
@@ -1,14 +1,27 @@
 const { emojis } = require('../../enum/EmojiEnum')
 
 const getCurrentDate = (withHTML = true) => {
-  var today = new Date()
-  var dd = String(today.getDate()).padStart(2, '0')
-  var mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
-  var yyyy = today.getFullYear()
+  const today = new Date()
+  const dd = String(today.getDate()).padStart(2, '0')
+  const mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+  const yyyy = today.getFullYear()
 
   const currentDate = dd + '/' + mm + '/' + yyyy
 
   return withHTML ? ('<b>' + emojis.calendar + ' ' + currentDate + '</b>') : currentDate
 }
 
-module.exports = { getCurrentDate }
+const isWeekend = (date) => {
+  const prohibitedWeekdays = {
+    sunday: 0,
+    saturday: 6
+  }
+
+  const today = date ? new Date(date) : new Date()
+  const dayOfWeek = today.getDay()
+  const result = dayOfWeek === prohibitedWeekdays.saturday || dayOfWeek === prohibitedWeekdays.sunday
+
+  return result
+}
+
+module.exports = { getCurrentDate, isWeekend }
