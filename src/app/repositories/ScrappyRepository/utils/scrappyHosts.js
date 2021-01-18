44aef7d5c8dba2524874a8f7b5444ba52e2bc0c4
Henrique Augusto <hick_97@hotmail.com>
2021-01-18 20:01:10 -0300

    feat: add a enum with dividends scrappy hosts

---
+++ src/app/repositories/ScrappyRepository/utils/scrappyHosts.js
@@ -0,0 +1,6 @@
+const dividendsScrappyHosts = {
+  stocks: 'https://statusinvest.com.br/acoes/proventos/ibovespa',
+  fiis: 'https://statusinvest.com.br/fundos-imobiliarios/proventos/ifix'
+}
+
+module.exports = { dividendsScrappyHosts }
