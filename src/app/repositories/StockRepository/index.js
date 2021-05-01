f56dff7fb5807210518ee8042e987409f0b2627a
Henrique Augusto <hick_97@hotmail.com>
2021-05-01 20:06:45 -0300

    fix: add b3 scrappy on findStock method

--- src/app/repositories/StockRepository/index.js
+++ src/app/repositories/StockRepository/index.js
@@ -5,10 +5,12 @@ const Api = require('../../services/api')
 const Wallet = require('../../models/Wallet')
 const Stock = require('../../models/Stock')
 
+const { scrappyLastStockDataUpdate } = require('../../repositories/ScrappyRepository')
+
 const { AlphaActions } = require('../../enum/AlphaVantageEnum')
 const { ActionMessages, ErrorMessages } = require('../../enum/MessagesEnum')
 
-const { stockAttributes } = require('./utils/stockAttributes')
+const { stockAttributes, defaultAttributes } = require('./utils/stockAttributes')
 
 class StockRepository {
   async listAllStocks() {
@@ -18,17 +20,19 @@ class StockRepository {
   }
 
   async createStock(share, stock) {
-    delete share[stockAttributes.MATCH_SCORE]
+    const isAlphaVantageData = !!share[stockAttributes.MATCH_SCORE]
+
+    isAlphaVantageData && delete share[stockAttributes.MATCH_SCORE]
 
     const obj = {
-      symbol: share[stockAttributes.SYMBOL],
-      name: share[stockAttributes.NAME],
-      type: share[stockAttributes.TYPE],
-      region: share[stockAttributes.REGION],
-      marketOpen: share[stockAttributes.MARKET_OPEN],
-      marketClose: share[stockAttributes.MARKET_CLOSE],
-      timezone: share[stockAttributes.TIMEZONE],
-      currency: share[stockAttributes.CURRENCY]
+      symbol: share[stockAttributes.SYMBOL] || `${stock.toUpperCase()}.SAO`,
+      name: share[stockAttributes.NAME] || defaultAttributes.NAME,
+      type: share[stockAttributes.TYPE] || defaultAttributes.TYPE,
+      region: share[stockAttributes.REGION] || defaultAttributes.REGION,
+      marketOpen: share[stockAttributes.MARKET_OPEN] || defaultAttributes.MARKET_OPEN,
+      marketClose: share[stockAttributes.MARKET_CLOSE] || defaultAttributes.MARKET_CLOSE,
+      timezone: share[stockAttributes.TIMEZONE] || defaultAttributes.TIMEZONE,
+      currency: share[stockAttributes.CURRENCY] || defaultAttributes.CURRENCY
     }
 
     const { _id } = await Stock.create({ ...obj, stock })
@@ -54,9 +58,19 @@ class StockRepository {
     const [match] = data.bestMatches
     const matchScore = match ? parseFloat(match.matchScore) : 0
 
-    const notFound = undefined
+    const notFound = matchScore < 0.5
 
-    if (matchScore < 0.5) return notFound
+    if (notFound) {
+      const stockToMatch = stock.toUpperCase()
+      const { stock: scrappedName } = await scrappyLastStockDataUpdate(stockToMatch)
+
+      const existsOnB3 = scrappedName === stockToMatch
+
+      if (!existsOnB3) return undefined
+
+      return true
+    }
+
     return match
   }
 }
