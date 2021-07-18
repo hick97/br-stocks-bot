ac5de401bee458ea02c78368acad24746a8786fa
Henrique Augusto <hick_97@hotmail.com>
2021-07-18 19:23:53 -0300

    feat: send ADMIN messages when API call fail

--- src/app/repositories/MessageRepository/index.js
+++ src/app/repositories/MessageRepository/index.js
@@ -4,10 +4,11 @@ const path = require('path')
 const FormData = require('form-data')
 
 const Api = require('../../services/api')
+
 const { useSentryLogger } = require('../../helpers/LogHelper')
 
 class MessageRepository {
-  async sendCustomMessage({ chat_id, text, message_id, options }) {
+  async sendCustomMessage({ chat_id, text, message_id, options, action }) {
     try {
       await axios.post(`${Api.telegramURL}/sendMessage`, {
         text,
@@ -15,9 +16,23 @@ class MessageRepository {
         parse_mode: 'HTML',
         reply_to_message_id: message_id || '',
         ...options
-
       })
     } catch (error) {
+      if (process.env.WITH_ADMIN_DEBUG && error.response && action) {
+        const adminText =
+        '<b>levelname: ERROR</b>\n\n' +
+        `<b>CHAT_ID = </b><code>${chat_id}</code>\n` +
+        `<b>ACTION = </b><code>${action}</code>\n` +
+        `<b>STATUS = </b><code>${error.response.status}(${error.response.statusText})</code>\n\n` +
+        `<b>DESCRIPTION</b>\n<code>${error.response.data.description}</code>\n`
+
+        await axios.post(`${Api.telegramURL}/sendMessage`, {
+          text: adminText,
+          chat_id: process.env.ADMIN_CHAT_ID,
+          parse_mode: 'HTML'
+        })
+      }
+
       useSentryLogger(error, `Falha ao enviar mensagem para o chat_id=${chat_id} e text=${text}`)
     }
   }
